83e720826ef6e62db7fd48988f3c38ea2d3d168e
Rakesh Sripelli <rakesh.sripelli@rampgroup.com>
2023-12-28 13:37:58 +0530

    'batfileadded'

---
+++ app.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Replace 'your_robot_file.robot' with the actual name of your Robot Framework test script
+robot C:\Users\RakeshSripelli\Desktop\CAT_FRAMEWORK\app.robot
+timeout /t 5 /nobreak > nul
